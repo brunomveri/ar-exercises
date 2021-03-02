0b950211b2759428a9fac95de04e6cfe6423468c
vagrant <mveri.bruno@gmail.com>
2021-03-02 02:22:51 +0000

    Finished exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.name = "Rhaissa's"
+@store1.save
+
+pp Store.find(1)
+
+@store1.update(name: "Bruno's Store")
+
+pp Store.find(1)
